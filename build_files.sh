6624190063fa5ecf2a6b6d7e469beb0b64d80d6b
Siri-Nelapatla <65899019+Siri-Nelapatla@users.noreply.github.com>
2023-02-11 19:31:35 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD STATRT"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.oy collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.oy collectstatic --noinput --clear
 echo "BUILD END"
