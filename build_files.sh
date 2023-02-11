b9c814981b2908fcf4d4955977dab43340da32ae
Siri-Nelapatla <65899019+Siri-Nelapatla@users.noreply.github.com>
2023-02-11 19:29:08 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,31 +1,4 @@
-{
-    "version":2,
-    "builds":[
-        {
-            "src": "django_blog/wsgi.py",
-            "use": "@vercel/python",
-            "config": {"maxLambdaSize": "15mb","runtime":"python3.11"}
-
-        },
-        {
-            "src": "build_files.sh",
-            "use": "@vercel/static-build",
-            "config":{
-                "disDir": "staticfiles_build"
-            }
-        }
-    
-    ],
-    "routes":[
-        {
-            "src":"/statsic/(.*)",
-            "dest":"/static/$1"
-        },
-        {
-            "src":"/(.*)"
-            "dest": "django_blog/wsgi.py"
-        }
-    ]
-
-
-}
+echo "BUILD STATRT"
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.oy collectstatic --noinput --clear
+echo "BUILD END"
